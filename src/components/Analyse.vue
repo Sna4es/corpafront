2804e1c7a5c46d5a72543381a9074adc0e4f91c6
Sna4es <Sna4es@gmail.com>
2020-06-17 22:59:14 +0400

    Мелкие правки, капля дизайна

--- src/components/Analyse.vue
+++ src/components/Analyse.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
-        <h3>Аналaйз</h3>
+        <h2>Страница анализа текста</h2>
         <form @submit.prevent="Analyse" class="analyse">
             <textarea v-model="text" placeholder="Введите текст для анализа"></textarea>
             <hr/>
             Замена символов <select v-model="doReplaces">
+            <hr/>
+            Замена символов
             <option v-for="choice in choices" v-bind:value="choice.option"> {{ choice.text }}</option>
         </select>
             <hr/>
@@ -45,6 +47,10 @@
                 atext: "1",
                 jtext: "51351",
                 qqtext:'',
+                qwtext: '',
+                qatext:'',
+                qbtext: '',
+                qctext: '',
                 ptext: '',
                 jstext: '',
                 params:'',
@@ -87,23 +93,52 @@
             },
             Replace: function () {
                 let qqtext = localStorage.getItem('analtext')
-                let qqext=qqtext.replace("слова","санёчек")
-                alert(qqext)
+                let qqext=qqtext.replace(/Форма слова/gi,',"lex" :')
+                let qwtext=qqext.replace(/Разбор:/gi, '"[{"Analysis" : {')
+                let qatext=qwtext.replace(/грамматика/gi, '"gr" :')
+                let qbtext=qatext.replace(/слово/gi, '}, "text" :')
+                alert(qbtext)
             },
             Parsing: function () {
-                let analtext = '3'
+                let analtext = ''
                 var analysis = ''
                 // var ptext = ''
                 // var
                 //     ptext=[{"analysis":[{"lex":"стол","gr":"S,m,inan=acc,sg"},{"lex":"стол","gr":"S,m,inan=nom,sg"}],"text":"стол"}]
-                var ptext = [{"analysis": [{"lex": "стол","gr": "S,m,inan=acc,sg"},{"lex": "стол","gr": "S,m,inan=nom,sg"}
+                // var ptext = [{"analysis": [{"lex": "стол","gr": "S,m,inan=acc,sg"},{"lex": "стол","gr": "S,m,inan=nom,sg"}
+                //         ],
+                //         "text": "стол"
+                //     },
+                //     {
+                //         "text": " "
+                //     },
+                //     { "analysis": [ { "lex": "стоять",  "gr": "V,ipf,intr=inpraes,sg,indic,3p" } ], "text": "стоит"},  {"text": "\n" } ]
+                var ptext = [
+                    {
+                        "analysis": [
+                            {
+                                "lex": "стоять",
+                                "gr": "V,ipf,intr=praet,sg,indic,f"
+                            }
                         ],
-                        "text": "стол"
+                        "text": "стояла"
                     },
                     {
-                        "text": " "
+                        "text": "\n "
                     },
-                    { "analysis": [ { "lex": "стоять",  "gr": "V,ipf,intr=inpraes,sg,indic,3p" } ], "text": "стоит"},  {"text": "\n" } ]
+                    {
+                        "analysis": [
+                            {
+                                "lex": "дверь",
+                                "gr": "S,f,inan=ins,sg"
+                            }
+                        ],
+                        "text": "дверью"
+                    },
+                    {
+                        "text": "\n"
+                    }
+                ]
                 // var ptext = [{"someKey":"blabla1"},{"key2":"blabla2"}]
                 //  this.ptext=localStorage.getItem('antext')
                 // console.log(ptext)
@@ -112,10 +147,14 @@
                     if (item.hasOwnProperty('analysis')) {
                     item['analysis'].forEach(function (etem,j,arrr) {
                         analtext +=' Разбор: форма слова '+ '"'+ etem['lex']+'"'
-                        analtext +='  грамматика  '+'"' + etem['gr']+'"'
+                        analtext +='  грамматика  '+'"' + etem['gr']+'" '
                     })
-                    localStorage.setItem('texyttwqtqwt', item['text'])
-                    analtext+= 'слово '+ item['text'] }
+                    analtext+= 'слово '+'"'+item['text']+'"' } else {
+                        // if (item['text'] = '" a"') {
+                        analtext += item['text']+'   {пробел}'}
+                    // }
+
+
 
                     //     analtext +='text'+ ptext.text
                     //     // item['text'].forEach(function (etim,k,arrrr) {
@@ -157,17 +196,40 @@
 </script>
 
 <style scoped>
-    textarea {
-        width: 25%;
-        height: 120px;
-        padding: 12px 20px;
-        box-sizing: border-box;
-        border: 2px solid #ccc;
-        border-radius: 12px;
-        background-color: #f8f8f8;
-        resize: both;
-        overflow: auto;
-    }
+    /*textarea {*/
+    /*    width: 25%;*/
+    /*    height: 120px;*/
+    /*    padding: 12px 20px;*/
+    /*    box-sizing: border-box;*/
+    /*    border: 2px solid #ccc;*/
+    /*    border-radius: 12px;*/
+    /*    background-color: #f8f8f8;*/
+    /*    resize: both;*/
+    /*    overflow: auto;*/
+    /*}*/
+select {
+    /*display: block;*/
+    font-size: 14px;
+    font-family: sans-serif;
+    font-weight: 700;
+    color: #444;
+    line-height: 1.3;
+    padding: .6em 1.4em .5em .8em; width: 100%;
+    max-width: 20%;
+    box-sizing: border-box;
+    margin: 0;
+    border: 1px solid #aaa;
+    box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
+    border-radius: .5em;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+    background-color: #fff;
+    background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'), linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
+    background-repeat: no-repeat, repeat;
+    background-position: right .7em top 50%, 0 0;
+    background-size: .65em auto, 100%;
+}
 
 
 </style>
